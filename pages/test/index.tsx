330057f2681d1b8ca87eca8bd74f4f06622f8823
김영선 <withbbang@gmail.com>
2023-07-27 14:38:22 +0900

    빌드 에러로 인한 커밋

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -3,7 +3,7 @@ import styles from "./Test.module.scss";
 function Test() {
   return (
     <>
-      <h1 className={styles.h1}>It's Test Page!</h1>
+      <h1 className={styles.h1}>It is Test Page!</h1>
     </>
   );
 }
