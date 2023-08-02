ce7b7dd8f39aa03ac14cb12cd8d51269053ae724
김영선 <withbbang@gmail.com>
2023-08-03 00:52:21 +0900

    ssr test

--- pages/test4/index.tsx
+++ pages/test4/index.tsx
@@ -13,8 +13,9 @@ export default function Test4() {
     ["test4"],
     queryTest4ClientSide,
     {
-      refetchOnWindowFocus: false, // 윈도우 클릭시 마다 데이터 리페칭 유무
-      refetchOnMount: false, // 서버사이드로 데이터 페칭 후 클라이언트사이드로 데이터 재페칭 유무
+      staleTime: 10 * 1000,
+      // refetchOnWindowFocus: false, // 윈도우 클릭시 마다 데이터 리페칭 유무
+      // refetchOnMount: false, // 서버사이드로 데이터 페칭 후 클라이언트사이드로 데이터 재페칭 유무
       // staleTime: Infinity, // Infinity로 할시 서버사이드로 데이터 페칭 후 클라이언트사이드로 데이터 재페칭 유무
     }
   );
@@ -32,7 +33,7 @@ export default function Test4() {
   );
 }
 
-export async function getServerSideProps()  {
+export const getServerSideProps: GetServerSideProps = async () => {
   const queryClient = new QueryClient();
   await queryClient.prefetchQuery(
     ["test4"],
