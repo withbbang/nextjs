f9890faec08531ee8770e779476d72a5a1cb69a0
김영선 <withbbang@gmail.com>
2023-08-03 11:31:52 +0900

    ssr 테스트

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
   i18n,
-  ...dev,
+  ...(process.env.NODE_ENV === "production" ? prod : dev),
 };
 
 module.exports = nextConfig;
