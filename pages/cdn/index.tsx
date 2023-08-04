5aea33b76fcfbd08e112a44e43777202864e0ea7
김영선 <withbbang@gmail.com>
2023-08-04 13:44:51 +0900

    cdn dynamic import 방식 수정

--- pages/cdn/index.tsx
+++ pages/cdn/index.tsx
@@ -1,28 +1,27 @@
-import { useEffect, useRef } from "react";
-import dynamic from "next/dynamic";
+import { useEffect } from "react";
+import Script from "next/script";
 import Title from "@/components/Title";
 import styles from "./CDN.module.scss";
 
-const Map = dynamic(() => import("../../components/Map"), { ssr: false });
-
 export default function CDN() {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.setAttribute(
-      "src",
-      "https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=clientId"
-    );
-    document.head.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
+    const map = new naver.maps.Map("map", {
+      center: new naver.maps.LatLng(37.3595704, 127.105399),
+      mapTypes: new naver.maps.MapTypeRegistry({
+        normal: naver.maps.NaverStyleMapTypeOptions.getVectorMap(),
+      }),
+    });
   }, []);
 
   return (
     <>
       <Title title={"CDN"} />
-      <Map />
+      <Script
+        strategy="beforeInteractive"
+        defer
+        src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=clientId"
+      />
+      <div className={styles.map} id="map"></div>
     </>
   );
 }
