43464437aeff5667c25fb156cf364367f4cea711
김영선 <withbbang@gmail.com>
2023-08-10 10:43:01 +0900

    scss 전역 설정

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
-
+const path = require("path");
 const prod = require("./configs/prod.config");
 const dev = require("./configs/dev.config");
 const { i18n } = require("./next-i18next.config");
 
 const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+    prependData: `@import "styles/_variables.scss"; @import "styles/_mixins.scss";`, // prependData 옵션 추가
+  },
   i18n,
   ...(process.env.NODE_ENV === "production" ? prod : dev),
 };
