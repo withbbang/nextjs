4fb31a3059a9b2054d35d7800ea986eb925490ee
김영선 <withbbang@gmail.com>
2023-11-10 01:27:23 +0900

    Style: 404 페이지 퍼블 중

--- pages/404/index.tsx
+++ pages/404/index.tsx
@@ -1,10 +1,30 @@
 import Title from "@/components/Title";
+import { useRouter } from "next/router";
+import styles from "./404.module.scss";
 
-export default function Test1() {
+export default function NotFoundPage() {
+  const router = useRouter();
+
+  const handleBack = () => {
+    router.back();
+  };
+
   return (
     <>
       <Title title={"Not Found Page"} />
-      <h1>404 Not Found Page</h1>
+      <div className={styles.wrap}>
+        <div className={styles.backBtn}>
+          <span onClick={handleBack}>
+            <SVG type="back" width="30px" height="30px" />
+          </span>
+        </div>
+        <div className={styles.innerWrap}>
+          <h1>Sorry This Page Is Not Found</h1>
+          <span>
+            <SVG type="notFound" width="500px" height="500px" />
+          </span>
+        </div>
+      </div>
     </>
   );
 }
