b6299843a6465bbc894da42fe7391fb34cd47a52
김영선 <withbbang@gmail.com>
2023-11-10 17:21:06 +0900

    Feat: 모바일 카카오톡 인앱 브라우저에서 강제로 기기의 기본 브라우저로 띄우는 스크립트 설정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,7 @@ export default function Document() {
     <Html>
       <Head>
         {/* 모든 페이지에 여기서 선언한 메타 태그가 head에 들어감, 루트파일이기에 가능한 적은 코드만 넣어야함  */}
+        <script defer src="/force-to-move-browser.js" />
       </Head>
       <body>
         <Main />
