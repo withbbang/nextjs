e3f406874e47d666b8fb41757a81d2e382556c80
김영선 <withbbang@gmail.com>
2023-11-13 16:45:13 +0900

    Test: getServerSideProps 안에서 get API를 통하여 데이터 페칭 후 화면에 바로 내려주기 테스트

--- api/test4/index.ts
+++ api/test4/index.ts
@@ -1,9 +1,9 @@
-import { getAPI, postAPI, putAPI, deleteAPI } from "@/utils/apis";
+import { getAPI } from "@/utils/apis";
 
 export function queryTest4ClientSide() {
-  return getAPI("/test/bad");
+  return getAPI("/api/example");
 }
 
-export function queryTest4ServerSide() {
-  return getAPI("http://localhost:4000/test/good");
+export function queryTest4ServerSide(param: string) {
+  return getAPI(`http://localhost:4000/api/example?param=${param}`);
 }
---
+++ pages/test4/index.tsx
@@ -0,0 +1,24 @@
+/*********************************************************************************
+ ***** getServerSideProps과 get API를 이용하여 서버에서 가져온 데이터 바로 내려주기 테스트 *****
+ ********************************************************************************/
+import Title from "@/components/Title";
+import styles from "./Test4.module.scss";
+import { GetServerSideProps } from "next";
+import { getAPI } from "@/utils/apis";
+
+export default function Test4({ data }: any) {
+  return (
+    <>
+      <Title title={"Test4"} />
+      <h1 className={styles.h1}>{data.key}</h1>
+    </>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const data = await getAPI("http://localhost:4000/api/example");
+
+  return {
+    props: { data },
+  };
+};
